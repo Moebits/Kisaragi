10111c307cfa6a0146b62c3e2e7f591e3d97b8c0
Tenpi <pie4chan@gmail.com>
2019-06-30 16:30:01 -0400

    Added =>role command

--- commands/moderator/role.js
+++ commands/moderator/role.js
@@ -2,47 +2,48 @@ exports.run = async (client, message, args) => {
 
   const roleEmbed = client.createEmbed();
 
-  if (!args[2]) {
-    message.channel.send(roleEmbed
-      .setDescription("Correct usage is =>role <add> <del> [user] [role]"));
-      return;
+  if ((!args[2]) || (message.mentions.members.size === 0)) {
+    return message.channel.send(roleEmbed
+      .setDescription("You must type =>role <add or del> [user] [role]"));
+      
   } else {
 
+      const member = message.mentions.members.first();
+      const roleName = args[2]
+
+      var snowflake = /\d+/;
+      var roleID = roleName.substring(roleName.search(snowflake));
+      if (roleID.includes(">")) roleID = roleID.slice(0, -1);
+      var role = message.guild.roles.get(roleID);
+
       switch (args[0]) {
+
         case 'add': {
-          
-          if (message.mentions.members.size === 0) return message.channel.send(`<@${message.author.id}>, Please mention a user to give the role to.`);
-          const member = message.mentions.members.first();
-          const name = client.combineArgs(args, 2);
-          // Find the role on the guild.
-          const role = message.guild.roles.find(r => r.name === name);
-          // End the command if the bot cannot find the role on the server.
-          if (!role) return message.reply('I can\'t seem to find that role.');
-          try {
-            await member.addRole(role);
-            await message.channel.send(`I've added the ${name} role to ${member.displayName}.`);
-          } catch (e) {
-            console.log(e);
-          }
-          break;
+            try {
+              await member.addRole(role);
+              await message.channel.send(roleEmbed
+                .setDescription(`${member.displayName} now has the ${role} role!`));
+            } catch (e) {
+              console.log(e);
+              message.channel.send(roleEmbed
+                .setDescription(`The role **${roleName}** could not be found.`));
+            }
+
+            break;  
         }
     
-        case 'remove': {
-          // Check if the message mentions a user.
-          if (message.mentions.members.size === 0) return message.reply('Please mention a user to take the role from.');
-          const member = message.mentions.members.first();
-          // This is the name of the role. For example, if you do 'role -remove @York#2400 The Idiot Himself', the name of the role would be 'The Idiot Himself'.
-          const name = args.slice(1).join(' ');
-          // Find the role on the guild.
-          const role = message.guild.roles.find(r => r.name === name);
-          // End the command if the bot cannot find the role on the server.
-          if (!role) return message.reply('I can\'t seem to find that role.');
+        case 'del': {
           try {
             await member.removeRole(role);
-            await message.channel.send(`I've removed the ${name} role from ${member.displayName}.`);
+            await message.channel.send(roleEmbed
+              .setDescription(`${member.displayName} no longer has the ${role} role!`));
           } catch (e) {
             console.log(e);
+            message.channel.send(roleEmbed
+              .setDescription(`The role **${roleName}** could not be found.`));
           }
+
+          break;  
         }
       }
     }
