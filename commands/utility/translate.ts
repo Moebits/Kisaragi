cfb18554011578a71395f367d10869761b3c4f76
Tenpi <pie4chan@gmail.com>
2019-07-16 17:49:03 -0400

    Added =>translate

---
+++ commands/utility/translate.ts
@@ -0,0 +1,20 @@
+exports.run = async (client: any, message: any, args: string[]) => {
+
+    const translate = require('@vitalets/google-translate-api');
+    let translateText = client.combineArgs(args, 1);
+    let translateEmbed = client.createEmbed();
+
+    let result = await translate(translateText, {to: 'ja'});
+    if (result.from.language.iso === 'ja') {
+        let newResult = await translate(translateText, {to: 'en'});
+        translateEmbed
+        .setTitle(`**Translated Text** ${client.getEmoji("kannaCurious")}`)
+        .setDescription(newResult.text)
+        message.channel.send(translateEmbed)
+        return;
+    }
+    translateEmbed
+    .setTitle(`**Translated Text** ${client.getEmoji("kannaCurious")}`)
+    .setDescription(result.text)
+    message.channel.send(translateEmbed)
+}
