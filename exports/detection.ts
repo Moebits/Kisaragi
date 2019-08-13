31678faf1e4eddc8858ff30a6452bd4cefc57c4e
Tenpi <37512637+Tenpi@users.noreply.github.com>
2019-08-13 15:02:25 -0400

    Detection update

--- exports/detection.ts
+++ exports/detection.ts
@@ -1,7 +1,8 @@
 module.exports = async (client: any, message: any) => {
 
-    let cv = require("opencv");
+    const cv = require('opencv4nodejs');
     const download = require('image-downloader')
+    const classifier = new cv.CascadeClassifier("../assets/cascades/animeface.xml");
 
     client.detectAnime = async (msg: any) => {
         if (msg.author.id === client.user.id) return;
@@ -10,14 +11,13 @@ module.exports = async (client: any, message: any) => {
             for (let i = 0; i < urls.length; i++) {
                 console.log(urls)
                 await download.image({url: urls[i], dest: `../assets/detection/image${i}.jpg`});
-                cv.readImage(`../assets/detection/image${i}.jpg`, function(err, im){
-                    im.detectObject("../assets/cascades/animeface.xml", {}, async function(err, faces) {
-                        if (!faces.join("")) {
-                            await msg.reply("You can only post anime pictures!");
-                            await msg.delete();
-                        }
-                    });
-                  });
+                const img = await cv.imreadAsync(`../assets/detection/image${i}.jpg`);
+                const grayImg = await img.bgrToGrayAsync();
+                const {objects} = await classifier.detectMultiScaleAsync(grayImg);
+                if (!objects.join("")) {
+                    await msg.reply("You can only post anime pictures!");
+                    await msg.delete();
+                }
             }
         }
     }
