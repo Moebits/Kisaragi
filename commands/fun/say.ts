4806296139d97c2af8099ecd0d6024b20091a3a1
Tenpi <37512637+Tenpi@users.noreply.github.com>
2019-08-23 11:57:21 -0400

    update

--- commands/fun/say.ts
+++ commands/fun/say.ts
@@ -1,7 +1,8 @@
 exports.run = async (discord: any, message: any, args: string[]) => {
 
     let rawText = discord.combineArgs(args, 1);
-    message.channel.send(discord.checkChar(rawText, 2000, "."));
+    await message.channel.send(discord.checkChar(rawText, 2000, "."));
+    await message.delete(1000);
     return;
     
 }
