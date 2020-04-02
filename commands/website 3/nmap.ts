bffadb5a450ed14d5033ef438ca54ed9ffcbe129
Tenpi <37512637+Tenpi@users.noreply.github.com>
2020-04-02 05:16:32 -0400

    yelp

---
+++ commands/website 3/nmap.ts
@@ -0,0 +1,83 @@
+import {Message, MessageEmbed} from "discord.js"
+import nmap from "node-nmap"
+import {Command} from "../../structures/Command"
+import {Embeds} from "../../structures/Embeds"
+import {Functions} from "../../structures/Functions"
+import {Kisaragi} from "../../structures/Kisaragi"
+
+export default class Nmap extends Command {
+    private readonly headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"}
+    constructor(discord: Kisaragi, message: Message) {
+        super(discord, message, {
+            description: "Scans a network using nmap.",
+            help:
+            `
+            \`nmap host/ip\` - Scans the network
+            `,
+            examples:
+            `
+            \`=>nmap google.com\`
+            `,
+            aliases: [],
+            cooldown: 10
+        })
+    }
+
+    public run = async (args: string[]) => {
+        const discord = this.discord
+        const message = this.message
+        const embeds = new Embeds(discord, message)
+        const text = Functions.combineArgs(args, 1)
+        if (!text) {
+            return this.noQuery(embeds.createEmbed()
+            .setAuthor("nmap", "https://miro.medium.com/max/450/1*e0PvOyJqlUEGd8h3WHWEnA.jpeg", "https://nmap.org/")
+            .setTitle(`**Nmap Scan** ${discord.getEmoji("kannaSpook")}`), "You must specify a host name or ip address.")
+        }
+        if (/127.0.0.1|localhost/gi.test(text) && message.author.id !== process.env.OWNER_ID) {
+            return message.reply(`Nice try, hackerman ${discord.getEmoji("kannaFU")}`)
+        }
+        const quickscan = new nmap.NmapScan(text)
+
+        let data = "" as any
+        quickscan.startScan()
+        await new Promise((resolve) => {
+            quickscan.on("complete", (d: any) => {
+                data = d
+                resolve()
+            })
+            quickscan.on("error", (err: Error) => {
+                return this.invalidQuery(embeds.createEmbed()
+                .setAuthor("nmap", "https://miro.medium.com/max/450/1*e0PvOyJqlUEGd8h3WHWEnA.jpeg", "https://nmap.org/")
+                .setTitle(`**Nmap Scan** ${discord.getEmoji("kannaSpook")}`))
+            })
+        })
+        const nmapArray: MessageEmbed[] = []
+        for (let i = 0; i < data.length; i++) {
+            const info = data[i]
+            if (!info.hostname) continue
+            const ports = info.openPorts ? "\n" + info.openPorts.map((p: any) => {
+                return `_Port_: **${p.port}**\n` +
+                `_Service:_ **${p.service}**\n`
+            }).join("") : "None"
+            const nmapEmbed = embeds.createEmbed()
+            nmapEmbed
+            .setAuthor("nmap", "https://miro.medium.com/max/450/1*e0PvOyJqlUEGd8h3WHWEnA.jpeg", "https://nmap.org/")
+            .setTitle(`**Nmap Scan** ${discord.getEmoji("kannaSpook")}`)
+            .setURL(`https://${info.hostname}`)
+            .setDescription(
+                `${discord.getEmoji("star")}_Hostname:_ **${info.hostname}**\n` +
+                `${discord.getEmoji("star")}_IP Address:_ **${info.ip}**\n` +
+                `${discord.getEmoji("star")}_Mac Address:_ ${info.mac ? `**${info.mac}**` : "Unavailable"}\n` +
+                `${discord.getEmoji("star")}_OS:_ ${info.osNmap ? `**${info.osNmap}**` : "Unavailable"}\n` +
+                `${discord.getEmoji("star")}_Open Ports:_ ${ports}\n`
+            )
+            nmapArray.push(nmapEmbed)
+        }
+
+        if (nmapArray.length === 1) {
+            message.channel.send(nmapArray[0])
+        } else {
+            embeds.createReactionEmbed(nmapArray)
+        }
+    }
+}
---
+++ commands/website 3/yelp.ts
@@ -0,0 +1,81 @@
+import axios from "axios"
+import {Message, MessageEmbed} from "discord.js"
+import {Command} from "../../structures/Command"
+import {Embeds} from "../../structures/Embeds"
+import {Functions} from "../../structures/Functions"
+import {Kisaragi} from "../../structures/Kisaragi"
+
+export default class Yelp extends Command {
+    constructor(discord: Kisaragi, message: Message) {
+        super(discord, message, {
+            description: "Searches for businesses on yelp.",
+            help:
+            `
+            _Note: Put the location in brackets. Default is New York City._
+            \`yelp query [location?]\` - Search for businesses in the location
+            `,
+            examples:
+            `
+            \`=>yelp mcdonalds\`
+            `,
+            aliases: [],
+            random: "none",
+            cooldown: 10
+        })
+    }
+
+    public run = async (args: string[]) => {
+        const discord = this.discord
+        const message = this.message
+        const embeds = new Embeds(discord, message)
+        const text = Functions.combineArgs(args, 1)
+        const location = text?.match(/(?<=\[)(.*?)(?=\])/)?.[0] ?? "New York City"
+        const term = text?.replace(/(\[)(.*?)(\])/, "").trim() ?? ""
+
+        const headers = {
+            "authorization": `Bearer ${process.env.YELP_API_KEY}`,
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36"
+        }
+
+        const json = await axios.get(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=review_count`, {headers}).then((r) => r.data)
+
+        const yelpArray: MessageEmbed[] = []
+        for (let i = 0; i < json.businesses.length; i++) {
+            const business = json.businesses[i]
+            const reviewJSON = await axios.get(`https://api.yelp.com/v3/businesses/${business.id}/reviews`, {headers}).then((r) => r.data.reviews)
+            let reviews = `${discord.getEmoji("star")}_Reviews:_\n`
+            for (let j = 0; j < reviewJSON.length; j++) {
+                const review = reviewJSON[j]
+                reviews += `**${review.user.name}** - ${review.text}\n`
+            }
+            const yelpEmbed = embeds.createEmbed()
+            .setAuthor("yelp", "https://www.freepnglogos.com/uploads/yelp-logo-17.png", "https://www.yelp.com/")
+            .setTitle(`**Yelp Search** ${discord.getEmoji("aquaUp")}`)
+            .setThumbnail(business.image_url)
+            .setURL(business.url)
+            .setDescription(
+                `${discord.getEmoji("star")}_Name:_ **${business.name}**\n` +
+                `${discord.getEmoji("star")}_Address:_ \`${business.location.display_address.join(" ")}\`\n` +
+                `${discord.getEmoji("star")}_Coordinates:_ \`${business.coordinates.latitude.toFixed(2)}, ${business.coordinates.longitude.toFixed(2)}\`\n` +
+                `${discord.getEmoji("star")}_Phone Number:_ \`${business.display_phone}\`\n` +
+                `${discord.getEmoji("star")}_Price:_ **${business.price}**\n` +
+                `${discord.getEmoji("star")}_Ratings:_ **${business.rating}**\n` +
+                `${discord.getEmoji("star")}_Review Count:_ **${business.review_count}**\n` +
+                `${discord.getEmoji("star")}_Transactions:_ ${business.transactions?.[0] ? business.transactions.join(", ") : "N/A"}\n` +
+                Functions.checkChar(reviews, 1000, "")
+            )
+            yelpArray.push(yelpEmbed)
+        }
+
+        if (!yelpArray[0]) {
+            return this.invalidQuery(embeds.createEmbed()
+            .setAuthor("yelp", "https://www.freepnglogos.com/uploads/yelp-logo-17.png", "https://www.yelp.com/")
+            .setTitle(`**Yelp Search** ${discord.getEmoji("aquaUp")}`))
+        }
+        if (yelpArray.length === 1) {
+            message.channel.send(yelpArray[0])
+        } else {
+            embeds.createReactionEmbed(yelpArray)
+        }
+    }
+}
